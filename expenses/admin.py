dbf2e57c9b90829bf68b664b87caaf4d42985df8
Sagorika Bhui <bhuisagorika@gmail.com>
2026-06-30 12:17:55 +0530

    Updated landing page color palette to match app theme and registered models in Django admin panel

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Expense, ExpenseSplit, Settlement
 
-# Register your models here.
+admin.site.register(Expense)
+admin.site.register(ExpenseSplit)
+admin.site.register(Settlement)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models herefrom django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "phone",
+        "upi_id",
+        "is_staff",
+    )
